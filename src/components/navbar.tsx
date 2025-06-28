f13139b3767cd8942602176729f8506118d75cba
Maciel <macielsemr@gmail.com>
2025-06-28 15:05:06 -0300

    feat: criado navbar, pendente de responsividade e modo mobile

---
+++ src/components/navbar.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+// import { ItemMenu } from "./item-menu";
+import { Home, BarChart3, Camera, Settings, LogIn } from "lucide-react";
+export function NavBar() {
+  return (
+    <div>
+      <nav
+        className="bg-gradient-to-r from-amarelo-base to-amarelo-terc flex justify-between
+       px-10 h-12 items-center shadow-xl"
+      >
+        <div className="w-12 h-full relative">
+          <Image src="/logo/logo_CID.png" color="white" alt="logo CID" fill />
+        </div>
+        <div className=" h-10 flex items-center gap-4">
+          <div
+            className="flex gap-2 items-center justify-center min-w-20 text-white 
+          rounded-sm p-1 text-sm hover:bg-laranja/75 transition-all duration-200"
+          >
+            <Home width={20} />
+            Home
+          </div>
+          <div
+            className="flex gap-2 items-center justify-center min-w-20 text-white 
+          rounded-sm p-1 text-sm hover:bg-laranja/75 transition-all duration-200"
+          >
+            <BarChart3 width={20} />
+            Dashboard
+          </div>
+          <div
+            className="flex gap-2 items-center justify-center min-w-20 text-white 
+          rounded-sm p-1 text-sm hover:bg-laranja/75 transition-all duration-200"
+          >
+            <Camera width={20} />
+            Galeria
+          </div>
+          <div
+            className="flex gap-2 items-center justify-center min-w-20 text-white 
+          rounded-sm p-1 text-sm hover:bg-laranja/75 transition-all duration-200"
+          >
+            <Settings width={20} />
+            Controle
+          </div>
+          <div
+            className="flex gap-2 items-center justify-center min-w-20 text-white 
+          rounded-sm p-1 text-sm hover:bg-laranja/75 transition-all duration-200"
+          >
+            <LogIn width={20} />
+            Entrar
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
