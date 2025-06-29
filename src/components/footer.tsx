f4f18cbf434e1f24a1d74e2b78a566c5cde42199
Maciel <macielsemr@gmail.com>
2025-06-29 12:03:59 -0300

    feat: criado footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,105 @@
+import { Home, BarChart3, Camera, Settings, LogIn } from "lucide-react";
+import Link from "next/link";
+
 export function Footer() {
   return (
-    <footer className="bg-preto text-cinza p-10 grid-cols-3 grid-rows-2">
-      <div>Infos</div>
-      <div>links rápidos</div>
-      <div>info suporte</div>
-      <div>creditos</div>
+    <footer className="bg-preto text-cinza p-5 w-screen absolute bottom-0">
+      <section className="flex flex-col md:flex-row md:justify-between gap-7 my-7 ">
+        <div className="flex flex-col gap-5 md:flex-1/3">
+          <h3 className="text-amarelo-sec font-bold text-lg">CID Manager</h3>
+          <p>
+            Sistema inteligente de gerenciamento para o robô agricultor CID.
+            Monitore, controle e otimize suas operações agrícolas com tecnologia
+            de ponta.
+          </p>
+          <div className="flex gap-5">
+            <span
+              className="w-8 h-8 bg-amarelo-base text-white flex items-center
+            justify-center rounded-full"
+            >
+              C
+            </span>
+            <span
+              className="w-8 h-8 bg-amarelo-sec text-white flex items-center
+            justify-center rounded-full"
+            >
+              I
+            </span>
+            <span
+              className="w-8 h-8 bg-amarelo-terc text-white flex items-center
+            justify-center rounded-full"
+            >
+              D
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3 className="font-bold text-white text-lg mb-2">Navegação</h3>
+          <Link
+            href={"/home"}
+            className="flex gap-2 items-center hover:cursor-pointer
+             hover:text-amarelo-sec w-32"
+          >
+            <Home width={20} />
+            Home
+          </Link>
+          <Link
+            href={"/dashboard"}
+            className="flex gap-2 items-center hover:cursor-pointer
+             hover:text-amarelo-sec w-32"
+          >
+            <BarChart3 width={20} />
+            Dashboard
+          </Link>
+          <Link
+            href={"/galeria"}
+            className="flex gap-2 items-center hover:cursor-pointer
+             hover:text-amarelo-sec w-32"
+          >
+            <Camera width={20} />
+            Galeria
+          </Link>
+          <Link
+            href={"/controle"}
+            className="flex gap-2 items-center hover:cursor-pointer
+             hover:text-amarelo-sec w-32"
+          >
+            <Settings width={20} />
+            Controle
+          </Link>
+          <Link
+            href={"/"}
+            className="flex gap-2 items-center hover:cursor-pointer 
+            hover:text-amarelo-sec w-32"
+          >
+            <LogIn width={20} />
+            Entrar
+          </Link>
+        </div>
+        <div className=" ">
+          <h3 className="font-bold text-lg text-white mb-2">Suporte</h3>
+          <div className="flex flex-col gap-1">
+            <p>
+              Status do Sistema: <span className="text-verde">Online</span>
+            </p>
+            <p>Versão: v1.0.0</p>
+            <p>
+              Última Atualização: <span>Hoje</span>
+            </p>
+            <h4 className="text-amarelo-sec font-medium">
+              Robô CID Status:{" "}
+              <span className="text-verde font-normal">Operacional</span>
+            </h4>
+          </div>
+        </div>
+      </section>
+      <div
+        className=" text-sm flex gap-2 flex-col lg:flex-row lg:justify-between text-center
+       py-7 border-t-1 border-cinza"
+      >
+        <p>&copy; 2025 CID Manager. Todos os direitos reservados</p>
+        <p>Desenvolvido para o projeto de Tecnologias Web 2</p>
+      </div>
     </footer>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,52 +1,58 @@
 import Image from "next/image";
 // import { ItemMenu } from "./item-menu";
 import { Home, BarChart3, Camera, Settings, LogIn } from "lucide-react";
+import Link from "next/link";
 export function NavBar() {
   return (
     <div>
       <nav
         className="bg-gradient-to-r from-amarelo-base to-amarelo-terc flex justify-between
-       px-10 h-12 items-center shadow-xl"
+       px-10 h-12 items-center shadow-xl w-screen "
       >
         <div className="w-12 h-full relative">
           <Image src="/logo/logo_CID.png" color="white" alt="logo CID" fill />
         </div>
         <div className=" h-10 flex items-center gap-4">
-          <div
+          <Link
+            href={"/home"}
             className="flex gap-2 items-center justify-center min-w-20 text-white 
           rounded-sm p-1 text-sm hover:bg-laranja/75 transition-all duration-200"
           >
             <Home width={20} />
             Home
-          </div>
-          <div
+          </Link>
+          <Link
+            href={"/dashboard"}
             className="flex gap-2 items-center justify-center min-w-20 text-white 
           rounded-sm p-1 text-sm hover:bg-laranja/75 transition-all duration-200"
           >
             <BarChart3 width={20} />
             Dashboard
-          </div>
-          <div
+          </Link>
+          <Link
+            href={"/galeria"}
             className="flex gap-2 items-center justify-center min-w-20 text-white 
           rounded-sm p-1 text-sm hover:bg-laranja/75 transition-all duration-200"
           >
             <Camera width={20} />
             Galeria
-          </div>
-          <div
+          </Link>
+          <Link
+            href={"/controle"}
             className="flex gap-2 items-center justify-center min-w-20 text-white 
           rounded-sm p-1 text-sm hover:bg-laranja/75 transition-all duration-200"
           >
             <Settings width={20} />
             Controle
-          </div>
-          <div
+          </Link>
+          <Link
+            href={"/"}
             className="flex gap-2 items-center justify-center min-w-20 text-white 
           rounded-sm p-1 text-sm hover:bg-laranja/75 transition-all duration-200"
           >
             <LogIn width={20} />
             Entrar
-          </div>
+          </Link>
         </div>
       </nav>
     </div>
